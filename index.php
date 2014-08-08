8d6e08faa529d53d94f70962f00cf53cf83a58cd
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2014-08-08 18:51:44 +0200

    g

--- index.php
+++ index.php
@@ -52,7 +52,8 @@ if(isset($_GET['v'])){
                       }).responseText;
 				
 				var url = $.parseJSON(response);
-				response = url[0];
+				response = url[0][0];
+
 				  
 				/**
 				
@@ -87,7 +88,7 @@ if(isset($_GET['v'])){
 							async: false
 							}).responseText;
 							var url = $.parseJSON(response);
-							response = url[0];
+							response = url[0][0];
 							console.log(response);
 							ytplayer.loadVideoById(response);
 							},500);
