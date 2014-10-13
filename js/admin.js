b3a249112d61b41ff88391decfef83df54c39b6d
KasperRT <kasper@kasperrt.no>
2014-10-14 00:44:32 +0200

    Catchfunction for form
    Added a catchfunction for the admin form, temporarily it only logs out the values.

--- js/admin.js
+++ js/admin.js
@@ -19,3 +19,17 @@ function admin()
 	}
 	$("#playlist").toggleClass("lowOpacity");
 }
+
+
+function submitAdmin(form)
+{
+	vote = form.vote.checked,
+	addSongs = form.addSongs.checked,
+	longSongs = form.longSongs.checked,
+	onlyMusic = form.onlyMusic.checked;
+	
+	console.log("Vote: "+vote);
+	console.log("Add Songs: "+addSongs);
+	console.log("Long Songs: "+longSongs);
+	console.log("Only music: "+onlyMusic);
+}
