d61b4fa0a48e8104dca6dd81747e2d7eb635546b
Nicolas <nixolas1@users.noreply.github.com>
2014-10-15 22:14:10 +0200

    Revert kasperrt shit

--- js/youtube.js
+++ js/youtube.js
@@ -122,13 +122,6 @@ function onPlayerStateChange(newState) {
 		wasPaused = false;
 		syncInterval = setInterval(getTime, 5000);
 		getTime();
-	}else if(newState.data == 1 && wasPaused)
-	{
-		console.log("unpaused");
-		beginning = false;
-		wasPaused = false;
-		//syncInterval = setInterval(getTime, 5000);
-		//getTime();
 	}else if(newState.data == 2)
 	{
 		clearInterval(syncInterval);
