c4739918f24b8c6cb2b5dab633a128f264188ea1
KasperRT <kasper@kasperrt.no>
2014-10-20 15:40:08 +0200

    Fixed the name of the remove after play box

--- php/panel.php
+++ php/panel.php
@@ -7,7 +7,7 @@
 	<label>Allow long songs 			<input type="checkbox" name="longSongs" value="1"></input></label>
 	<label>Show playlist on frontpage 	<input type="checkbox" name="frontPage" value="1"></input></label>
 	<label>Allow only music 			<input type="checkbox" name="onlyMusic" value="1"></input></label>
-	<label>Remove song after playing 	<input type="checkbox" name="onlyMusic" value="1"></input></label>
+	<label>Remove song after playing 	<input type="checkbox" name="removePlay" value="1"></input></label>
 
 	<input type="button" class="button" value="Save Settings" onclick="submitAdmin(this.form)">   
 </form>
