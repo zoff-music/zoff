13d47a000a4b22383df028731fa527af980dadaf
Nicolas A. Tonne <nixatto@gmail.com>
2014-10-20 15:36:06 +0200

    Forgot to add some files

---
+++ js/toggler.js
@@ -0,0 +1,20 @@
+/*var e = document.getElementById("filt-css"),
+    d = document.getElementById("filt-javascript"),
+    t = document.getElementById("switcher");
+
+e.addEventListener("click", function(){
+  t.checked = false;
+  e.classList.add("toggler--is-active");
+  d.classList.remove("toggler--is-active");
+});
+
+d.addEventListener("click", function(){
+  t.checked = true;
+  d.classList.add("toggler--is-active");
+  e.classList.remove("toggler--is-active");
+});
+
+t.addEventListener("click", function(){
+  d.classList.toggle("toggler--is-active");
+  e.classList.toggle("toggler--is-active");
+})*/
