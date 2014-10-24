21d768434639fcb5fa5485e9db60b1f3bc5344b7
Nicolas A. Tonne <nixatto@gmail.com>
2014-10-24 15:29:24 +0200

    More panel settings

--- php/panel.php
+++ php/panel.php
@@ -1,7 +1,7 @@
 <b>Admin Panel</b>
 
 <form id="adminForm" onsubmit="return false" name="ufo" action="" class="daform nomargin" id="base">
-
+    <div class="toggles">
     <div class="toggler">
         <label><input type="radio" class="radio"name="frontPage" value="1"><span>Display</span></label> / 
         <label><input type="radio" class="radio"name="frontPage" value="0"><span>Hide</span></label>
@@ -26,19 +26,20 @@
         <label><input type="radio" class="radio"name="allvideos" value="1"><span>All</span></label> / 
         <label><input type="radio" class="radio"name="allvideos" value="0"><span>Song</span></label>
     </div>
-        
+
     <div class="toggler">
         <label><input type="radio" class="radio"name="removePlay" value="1"><span>Remove</span></label> / 
         <label><input type="radio" class="radio"name="removePlay" value="0"><span>Keep</span></label>
     </div>
+    </div>
 
-    <div>
-        playlist on frontpage
-        can vote
-        can add songs
-        long songs
-        categories allowed
-        song after player
+    <div class="toggtext">
+        playlist on frontpage<br>
+        can vote<br>
+        can add songs<br>
+        <span title="(>12 min)">long songs</span><br>
+        categories allowed<br>
+        songs after playing<br>
     </div>
 
 	<input type="password" name="pass" id="passbox" class="passbox" placeholder="Channel password">
