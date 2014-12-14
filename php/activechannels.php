89de5e617f9a54ee97e875f259eb84d6b35e1089
KasperRT <kasperr@stud.ntnu.no>
2014-12-14 14:03:09 +0100

    Update activechannels.php

--- php/activechannels.php
+++ php/activechannels.php
@@ -6,13 +6,13 @@ $all_channels = array();
 $time = 60*60*24*4; //4 dager
 foreach($dir as $files){
 	if(strpos($files, '.json') !== FALSE){
-		if(time() - filemtime('./lists/'.$files) < $time){
+		if(time() - filemtime('../lists/'.$files) < $time){
 			array_push($channels, ucfirst(str_replace(".json", "", $files)));
 		}
 		array_push($all_channels, ucfirst(str_replace(".json", "", $files)));
 	}
 }
 
-print_r(json_encode($all_channels));
+print_r(json_encode($channels));
 
 ?>
