f5b3094e974401273d4acc33359748d51636d50e
Kasper Rynning-Tønnesen <kassto2@hotmail.com>
2015-02-06 10:53:46 +0100

    Mobile design?

--- php/header.php
+++ php/header.php
@@ -21,10 +21,10 @@
 <meta name="author" content="Nicolas 'Nixo' Almagro Tonne &amp; Kasper 'KasperRT' Rynning-Tønnesen">
 <meta name="description" content="The shared YouTube radio.">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta property="og:image" content="static/Logo.png" />
+<meta property="og:image" content="static/l.png" />
 <meta property="og:title" content="Zöff">
 <meta property="og:description" content="The shared YouTube radio.">
 <meta property="og:type" content="website">
 <link rel="stylesheet" type="text/css" href="static/style.css" title="Default" />
-<link rel="icon" type="image/png" href="static/favicon.png"/>
+<link rel="icon" type="image/png" href="static/l.png"/>
 <link rel="stylesheet" type="text/css" href="static/controlstyle.css" />
