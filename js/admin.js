a0a08a9bbf580eddf862bd0588a865609a91c3d6
Kasper Rynning-Tønnesen <kassto2@hotmail.com>
2015-02-06 15:56:10 +0100

    fuck

--- js/admin.js
+++ js/admin.js
@@ -45,5 +45,6 @@ function submitAdmin(form)
 	setTimeout(function(){
 		$("#adminPanel").removeClass("success");
 		$("#adminPanel").removeClass("fadeerror");
+		$("#eBar").removeClass("opacityFull");
 	},1500);
 }
