2e3b2f4d4eef7fa5bbd22043ec7b031c777d57e1
Kasper Rynning-Tønnesen <kassto2@hotmail.com>
2015-02-13 00:56:47 +0100

    Fixed so datalist still contains all channels

--- php/nochan.php
+++ php/nochan.php
@@ -31,7 +31,7 @@ foreach($fil as $files){
 		if($time_lasted < $time){
 			$file = file_get_contents($files); //Checking if the channel has the setting for showing on the frontpage set to true.
 			$data = json_decode($file, TRUE);
-			if(!array_key_exists("frontpage", $data['conf']) || $data['conf']['frontpage'] == "true" && $i <= 14){ 						  //If it is true, the channelname will be shown on the frontpage
+			if(!array_key_exists("frontpage", $data['conf']) || $data['conf']['frontpage'] == "true" && $i <= 8){ 						  //If it is true, the channelname will be shown on the frontpage
 				array_push($channels, ucfirst(str_replace(".json", "", $files)));
 			}
 		}
