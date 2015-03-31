076f8e821fa47911cce281956fdce93df5b86de1
Nicolas A. Tonne <nixatto@gmail.com>
2015-03-31 20:30:25 +0200

    Piwik tracking added #106

--- php/nochan.php
+++ php/nochan.php
@@ -131,5 +131,20 @@ foreach($fil as $files){
          }
         });
 	</script>
+	<!-- Piwik tracking -->
+	<script type="text/javascript">
+	  var _paq = _paq || [];
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+	    var u="//zoff.no/analyse/";
+	    _paq.push(['setTrackerUrl', u+'piwik.php']);
+	    _paq.push(['setSiteId', 1]);
+	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<noscript><p><img src="//zoff.no/analyse/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
 	</body>
 </html>
