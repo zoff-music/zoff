266863452ba38d6949d99d6562d854dd84c7911a
Nicolas A. Tonne <nixatto@gmail.com>
2015-04-26 16:54:46 +0200

    Moar toasts, log out button

--- php/panel.php
+++ php/panel.php
@@ -14,7 +14,7 @@
                       <li class="white-bg">
                           <a class="white-bg">
                               <div class="input-field">
-                                <i class="mdi-action-lock"></i>
+                                <i id="admin-lock" class="mdi-action-lock" onclick="log_out()" title="Click to log out"></i>
                                   <input placeholder="Enter channel password" id="password" type="password" class="validate">
                               </div>
                           </a>
@@ -107,66 +107,3 @@
         </li>
     </ul>
 </li>
-
-
-
-
-<!--<span id="setpass"></span>
-
-<input type="button" class="button" value="Shuffle" onclick="shuffle(this.form);" title="Shuffle the playlist">
-
-<form id="adminForm" onsubmit="return false" name="ufo" action="" class="daform nomargin" id="base">
-    <div class="toggles">
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="frontpage" value="true"><span>Display</span></label> /
-        <label><input type="radio" class="radio" name="frontpage" value="false"><span>Hide</span></label>
-    </div>
-
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="vote" value="true"><span>Admin</span></label> /
-        <label><input type="radio" class="radio" name="vote" value="false"><span>Anyone</span></label>
-    </div>
-
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="addsongs" value="true"><span>Admin</span></label> /
-        <label><input type="radio" class="radio" name="addsongs" value="false"><span>Anyone</span></label>
-    </div>
-
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="longsongs" value="true"><span>Allow</span></label> /
-        <label><input type="radio" class="radio" name="longsongs" value="false"><span>Block</span></label>
-    </div>
-
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="allvideos" value="true"><span>All</span></label> /
-        <label><input type="radio" class="radio" name="allvideos" value="false"><span>Song</span></label>
-    </div>
-
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="removeplay" value="true"><span>Remove</span></label> /
-        <label><input type="radio" class="radio" name="removeplay" value="false"><span>Keep</span></label>
-    </div>
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="skip" value="true"><span>Allow</span></label> /
-        <label><input type="radio" class="radio" name="skip" value="false"><span>Block</span></label>
-    </div>
-    <div class="toggler">
-        <label><input type="radio" class="radio" name="shuffle" value="true"><span>Allow</span></label> /
-        <label><input type="radio" class="radio" name="shuffle" value="false"><span>Block</span></label>
-    </div>
-    </div>
-
-    <div class="toggtext">
-        playlist on frontpage<br>
-        can vote<br>
-        can add songs<br>
-        <span title="(>12 min)">long songs</span><br>
-        categories allowed<br>
-        songs after playing<br>
-        skipping<br>
-        shuffle<br>
-    </div>
-	<input type="password" name="pass" id="passbox" class="passbox" placeholder="Password">
-	<input type="submit" class="button" value="Login/Save" onclick="submitAdmin(this.form);" title="Save settings/Login">
-</form>
--->
