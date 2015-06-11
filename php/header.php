e5ecd2e0086d9c0fe2248504b0270dfd1ca955b1
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-06-11 16:22:59 +0200

    I don't like lowercase on names

--- php/header.php
+++ php/header.php
@@ -7,12 +7,12 @@
 
 <title>Zöff</title>
 <meta name="author" content="Nicolas 'Nixo' Almagro Tonne &amp; Kasper 'KasperRT' Rynning-Tønnesen">
-<meta name="description" content="The shared YouTube radio.">
+<meta name="description" content="The Shared (free) YouTube radio. Being built around the YouTube search and video API it enables the creation of collaborative and shared live playlists, with billions of videos and songs to choose from, all for free and without registration. Enjoy!">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta property="og:image" content="/static/images/highlogo.png" />
 <meta property="og:title" content="Zöff">
-<meta property="og:description" content="The shared YouTube radio.">
+<meta property="og:description" content="The Shared (free) YouTube radio. Being built around the YouTube search and video API it enables the creation of collaborative and shared live playlists, with billions of videos and songs to choose from, all for free and without registration. Enjoy!">
 <meta property="og:type" content="website">
 <link type="text/css" rel="stylesheet" href="/static/css/materialize.min.css"  media="screen,projection"/>
 <link rel="stylesheet" type="text/css" href="/static/css/style.css" title="Default" />
