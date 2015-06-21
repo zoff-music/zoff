2aecacf1390a4605a0172ac4982ab201448d8fb1
kasperrt <kasper@kasperrt.no>
2015-06-21 20:30:51 +0200

    Moved analytics to header

--- php/footer.php
+++ php/footer.php
@@ -84,14 +84,3 @@
 <script type="text/javascript" src="/static/js/chat.js"></script>
 <script type="text/javascript" src="/static/js/hostcontroller.js"></script>
 <!--<script type="text/javascript" src="/static/js/remotecontroller.js"></script>-->
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '***REMOVED***', 'auto');
-  ga('send', 'pageview');
-
-</script>
