00c17a38bba45189b6a258e519d77297226eb54b
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-06-24 17:41:19 +0200

    Changed github links

--- php/footer.php
+++ php/footer.php
@@ -19,7 +19,7 @@
                         <!--<a href="https://play.google.com/store/apps/details?id=no.lqasse.zoff">
                             <img title="Get it on Google Play" src="/static/images/google_play.png">
                         </a>-->
-                        <a href="https://github.com/nixolas1/Zoff">
+                        <a href="https://github.com/zoff-music/Zoff">
                             <img title="Contribute on GitHub" src="/static/images/GitHub_Logo.png" alt="GitHub" />
                         </a>
                         <p>
