fe35b4cf750bd27a20a30be1b8dcb78381472c4c
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-07-03 21:54:53 +0200

    Changed the loader in channel to flat preloader

--- index.php
+++ index.php
@@ -198,51 +198,9 @@
             </div>
             <div id="playlist" class="col s12 m3">
                 <div id="wrapper">
-                    <li id="playlist_loader" class="valign-wrapper">
-                      <div class="valign">
-                         <div class="preloader-wrapper big active">
-                          <div class="spinner-layer spinner-blue">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-red">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-yellow">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-green">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      </li>
+                    <div id="preloader" class="progress channel_preloader">
+                        <div class="indeterminate"></div>
+                    </div>
                     <div id="list-song-html">
                         <div id="list-song" class="card left-align list-song">
                             <span class="clickable vote-container" title="Vote!">
