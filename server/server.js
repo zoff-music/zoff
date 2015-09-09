38b93396886fa0cfae87a3d4a90b8585477300b6
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-09-09 12:50:44 +0200

    Making changes for cloudflare

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ var server;
   cors_proxy.createServer({
       requireHeader: ['origin', 'x-requested-with'],
       removeHeaders: ['cookie', 'cookie2'],
-      httpsOptions: credentials
+      //httpsOptions: credentials
   }).listen(8080, function() {
       console.log('Running CORS Anywhere on :' + 8080);
   });
