5dcee162cfb3151317f293592e160119725d2566
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-11-26 12:22:39 +0100

    Mobile fix looks

--- php/nochan.php
+++ php/nochan.php
@@ -101,6 +101,29 @@ if(isset($_GET['chan'])){
             </div>
     </div>
 
+    <div class="section mobile-search">
+            <form class="row" id="base" method="get">
+                    <div class="input-field col s12">
+                        <input
+                            class="input-field"
+                            type="text"
+                            id="search"
+                            name="chan"
+                            title="Type channel name here to create or listen to a channel. Only alphanumerical chars. [a-zA-Z0-9]+"
+                            autocomplete="off"
+                            list="searches"
+                            required pattern="[a-zA-Z0-9]+"
+                            spellcheck="false"
+                            maxlength="18"
+                            data-length="18"
+                        />
+                        <label for="search" class="noselect">Find or create radio channel</label>
+                        <datalist id="searches">
+                        </datalist>
+                </div>
+            </form>
+        </div>
+
     <main class="center-align container">
         <div class="section">
             <div id="preloader" class="progress">
