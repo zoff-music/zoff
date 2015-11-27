08a7457fb3c46c8833ba088854c1e7aef4ab57d9
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-11-27 17:22:30 +0100

    paused and playing

--- static/js/playercontrols.js
+++ static/js/playercontrols.js
@@ -17,6 +17,8 @@ var Playercontrols = {
     	document.getElementById("volume-button").addEventListener("click", Playercontrols.mute_video);
     	document.getElementById("playpause").addEventListener("click", Playercontrols.play_pause);
     	document.getElementById("fullscreen").addEventListener("click", Playercontrols.fullscreen);
+        ytplayer.pauseVideo();
+        Playercontrols.play_pause();
     },
 
     initSlider: function()
@@ -58,7 +60,6 @@ var Playercontrols = {
 
     play_pause: function()
     {
-        console.log(ytplayer.getPlayerState());
     	if(ytplayer.getPlayerState() == 1)
     	{
     		ytplayer.pauseVideo();
