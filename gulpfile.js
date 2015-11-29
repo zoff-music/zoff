fbb9f9b74554e09905722e6f7184503d7852a7ef
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-11-29 13:38:52 +0100

    added gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,40 @@ var gulp    = require('gulp'),
 	concat  = require('gulp-concat');
 
 gulp.task('js', function () {
-    gulp.src(['../static/js/*.js', '!../static/js/nochan*', '!../static/js/remotecontroller.js'])
+    gulp.src(['static/js/*.js', '!static/js/nochan*', '!static/js/remotecontroller.js'])
         .pipe(uglify({
         	mangle: true,
             compress: true,
         	enclose: true
         }))
         .pipe(concat('main.min.js'))
-        .pipe(gulp.dest('../static/dist'));
+        .pipe(gulp.dest('static/dist'));
 });
 
 gulp.task('nochan', function () {
-    gulp.src(['../static/js/nochan.js'])
+    gulp.src(['static/js/nochan.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('frontpage.min.js'))
-        .pipe(gulp.dest('../static/dist'));
+        .pipe(gulp.dest('static/dist'));
 });
 
 gulp.task('remotecontroller', function () {
-    gulp.src(['../static/js/remotecontroller.js'])
+    gulp.src(['static/js/remotecontroller.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('remote.min.js'))
-        .pipe(gulp.dest('../static/dist'));
+        .pipe(gulp.dest('static/dist'));
 });
 
 gulp.task('default', function(){
-    gulp.watch('../static/js/*.js', ['js']); 
-    gulp.watch('../static/js/nochan.js', ['nochan']);
-    gulp.watch('../static/js/remotecontroller.js', ['remotecontroller']);
+    gulp.watch('static/js/*.js', ['js']); 
+    gulp.watch('static/js/nochan.js', ['nochan']);
+    gulp.watch('static/js/remotecontroller.js', ['remotecontroller']);
 });
