89f85da02934e1c80940c85eec968c6c97e3e755
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-11-29 14:07:13 +0100

    Changed urls of remote

--- php/controller.php
+++ php/controller.php
@@ -13,7 +13,7 @@
                 </a>
                 <a href="//zoff.no" class="brand-logo hide-on-med-and-up">Zöff</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                  <li><a class="waves-effect green" title="Remote control a Zöff player" href="remote">Remote</a></li>
+                  <li><a class="waves-effect green" title="Remote control a Zöff player" href="https://remote.zoff.no">Remote</a></li>
                   <li><a class="modal-trigger waves-effect waves-orange" onclick="$('#about').openModal()">About</a></li>
                   <li><a class="modal-trigger waves-effect waves-yellow" onclick="$('#legal').openModal()">Legal</a></li>
                   <li><a class="waves-effect waves-purple" href="https://github.com/zoff-music/Zoff">GitHub</a></li>
--- php/panel.php
+++ php/panel.php
@@ -123,7 +123,7 @@
                             <h4 id="code-text">ABBADUR</h4>
                         </a>
                         <a>
-                          You can control this Zöff instance from another device by going to <b>http://zoff.no/remote</b>
+                          You can control this Zöff instance from another device by going to <b>http://remote.zoff.no/</b>
                           
                         </a>
                     </li>
