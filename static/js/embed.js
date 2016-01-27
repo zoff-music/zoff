3ad6c8e3ec5cf15a84d229b8c45e049ac5b31e8c
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-01-27 15:12:03 +0100

    wrong url

--- static/js/embed.js
+++ static/js/embed.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	console.log(document);
 	console.log(window.location.hash)
 
-	//add = "https://zoff.no";
+	add = "https://zoff.no";
 	add = "localhost";
 	socket = io.connect(''+add+':8880', connection_options);
 
