56fc07fa36baa0f72bf22b5f1329b90487b29a78
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-02-02 13:13:51 +0100

    Added listeners on frontpage

--- static/js/admin.js
+++ static/js/admin.js
@@ -91,7 +91,6 @@ var Admin = {
 
     	socket.on("conf", function(msg)
     	{
-            
             Crypt.init();
     		Admin.set_conf(msg[0]);
             if(Crypt.get_pass(chan.toLowerCase()) !== undefined && Admin.beginning && Crypt.get_pass(chan.toLowerCase()) != ""){
