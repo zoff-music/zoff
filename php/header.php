0e14399f8897111997e40ab7b2e45011350c032f
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-02-11 13:51:14 +0100

    Trying to fix header error

--- php/header.php
+++ php/header.php
@@ -17,7 +17,7 @@
 <meta property="og:description" content="The Shared (free) YouTube radio. Being built around the YouTube search and video API it enables the creation of collaborative and shared live playlists, with billions of videos and songs to choose from, all for free and without registration. Enjoy!"/>
 <meta property="og:type" content="website"/>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.min.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link tyle="text/css" rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 <link type="text/css" rel="stylesheet" href="/static/css/materialize.min.css" />
 <link rel="stylesheet" type="text/css" href="/static/css/style.css" title="Default" />
 <link rel="icon" id="favicon" type="image/png" href="/static/images/favicon.png"/>
