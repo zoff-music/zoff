f82ad4374918cdae612a45047cdd71fd4de63767
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-04-27 00:42:52 +0200

    Disabled serviceworker fully

--- static/js/listeners.js
+++ static/js/listeners.js
@@ -54,18 +54,6 @@ var connection_options = {
 	'force new connection': true 
 };
 
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(function (registration) {
-            console.log(registration);
-        })
-        .catch(function (e) {
-            console.error(e);
-        })
-} else {
-    console.log('Service Worker is not supported in this browser.');
-}
-
 $().ready(function(){
 	if(!window.fromFront && window.location.pathname != "/") init();
 });
