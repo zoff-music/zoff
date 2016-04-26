b1f5e307a13b467ff00caa74a1c450f592da476a
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-04-27 00:59:54 +0200

    Removed fetch event..

--- static/service-worker.js
+++ static/service-worker.js
@@ -33,13 +33,6 @@ self.addEventListener('activate', function (event) {
     )
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    );
-});
 
 function requestBackend(event){
     var url = event.request.clone();
