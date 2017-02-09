24f6723e3c8e576c49c9cbefd4cbd5216a3c8168
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-02-09 15:16:08 +0100

    White loader on channel-search

--- public/php/channel.php
+++ public/php/channel.php
@@ -30,47 +30,17 @@
                 <ul class="right control-list noselect">
                   <li id="search_loader" class="valign-wrapper hide">
                       <div class="valign">
-                         <div class="preloader-wrapper small active">
-                          <div class="spinner-layer spinner-blue">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-red">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-yellow">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-
-                          <div class="spinner-layer spinner-green">
-                            <div class="circle-clipper left">
-                              <div class="circle"></div>
-                            </div><div class="gap-patch">
-                              <div class="circle"></div>
-                            </div><div class="circle-clipper right">
-                              <div class="circle"></div>
-                            </div>
-                          </div>
-                        </div>
+                        <div class="preloader-wrapper small active">
+                         <div class="spinner-layer spinner-white-only">
+                           <div class="circle-clipper left">
+                             <div class="circle"></div>
+                           </div><div class="gap-patch">
+                             <div class="circle"></div>
+                           </div><div class="circle-clipper right">
+                             <div class="circle"></div>
+                           </div>
+                         </div>
+                       </div>
                       </div>
                       </li>
                     <li>
