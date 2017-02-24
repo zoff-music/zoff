9d6022a0b22e7cd4cbcc0e6281fd35c7c7acc9db
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-02-24 12:45:59 +0100

    Forgot to remove fireplace on channel-change..

--- public/js/listeners.js
+++ public/js/listeners.js
@@ -1600,6 +1600,9 @@ function onepage_load(){
 	    socket.emit("list", chan.toLowerCase());
 	}else if(url_split[3] === ""){
         clearTimeout(width_timeout);
+        ffireplace_initiated = false;
+        Player.fireplace.destroy();
+        $("#fireplace_player").css("display", "none");
 		$("#channel-load").css("display", "block");
 		window.scrollTo(0, 0);
 
