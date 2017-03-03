e59fcafe12d2f378f85caebdd9238d7cd3a4266f
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-03-03 11:08:42 +0100

    Static imagesize on result image, and fixed way of adding suggested number

--- public/js/suggestions.js
+++ public/js/suggestions.js
@@ -1,7 +1,11 @@
 var Suggestions = {
 
 	catchUserSuggests: function(params, single){
-		number_suggested = number_suggested + params.length;
+		if(single) {
+			number_suggested = number_suggested + 1;
+		} else {
+			number_suggested = number_suggested + params.length;
+		}
 		var to_display = number_suggested > 9 ? "9+" : number_suggested;
 		if($(".suggested-link span.badge.new.white").hasClass("hide") && number_suggested > 0){
 				$(".suggested-link span.badge.new.white").removeClass("hide");
