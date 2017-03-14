9ec188561cbbfa307ac823227508c59f7e75ff4f
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-03-14 23:18:12 +0100

    Trying fix for weird get-requests

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
     $guid=substr(base64_encode(crc32($_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_ACCEPT_LANGUAGE'])), 0, 8);
-    if(isset($_GET['__mref'])) {header('Location: /'); exit;}
+    if(!isset($_GET['chan'])) {header('Location: /'); exit;}
     else if(isset($_GET['chan'])) {header('Location: '.$_GET['chan']); exit;}
     $list = explode("/", htmlspecialchars(strtolower($_SERVER["REQUEST_URI"])));
     if($list[1]==""||!isset($list[1])||count($list)<=1){$list="";include('public/php/frontpage.php');die();}
