c9af8610735b73e6cd3f88c45b769dd89f8d0e40
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-05-03 19:08:51 +0200

    Gulpfile format

--- gulpfile.js
+++ gulpfile.js
@@ -4,65 +4,65 @@ var gulp    = require('gulp'),
 	concat  = require('gulp-concat');
 
 gulp.task('js', function () {
-    gulp.src(['server/views/assets/js/*.js', '!server/views/assets/js/embed*', '!server/views/assets/js/remotecontroller.js', '!server/views/assets/js/callback.js'])
+    gulp.src(['server/public/assets/js/*.js', '!server/public/assets/js/embed*', '!server/public/assets/js/remotecontroller.js', '!server/public/assets/js/callback.js'])
         .pipe(uglify({
         	mangle: true,
             compress: true,
         	enclose: true
         }))
         .pipe(concat('main.min.js'))
-        .pipe(gulp.dest('server/views/assets/dist'));
+        .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 gulp.task('embed', function () {
-    gulp.src(['server/views/assets/js/player.js', 'server/views/assets/js/helpers.js', 'server/views/assets/js/playercontrols.js', 'server/views/assets/js/list.js', 'server/views/assets/js/embed.js', '!server/views/assets/js/frontpage*', '!server/views/assets/js/remotecontroller.js'])
+    gulp.src(['server/public/assets/js/player.js', 'server/public/assets/js/helpers.js', 'server/public/assets/js/playercontrols.js', 'server/public/assets/js/list.js', 'server/public/assets/js/embed.js', '!server/public/assets/js/frontpage*', '!server/public/assets/js/remotecontroller.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('embed.min.js'))
-        .pipe(gulp.dest('server/views/assets/dist'));
+        .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 gulp.task('callback', function () {
-    gulp.src(['server/views/assets/js/callback.js'])
+    gulp.src(['server/public/assets/js/callback.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('callback.min.js'))
-        .pipe(gulp.dest('server/views/assets/dist'));
+        .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 /*
 gulp.task('nochan', function () {
-    gulp.src(['server/views/assets/js/nochan.js', 'server/views/assets/js/helpers.js'])
+    gulp.src(['server/public/assets/js/nochan.js', 'server/public/assets/js/helpers.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('frontpage.min.js'))
-        .pipe(gulp.dest('server/views/assets/dist'));
+        .pipe(gulp.dest('server/public/assets/dist'));
 });*/
 
 gulp.task('remotecontroller', function () {
-    gulp.src(['server/views/assets/js/remotecontroller.js'])
+    gulp.src(['server/public/assets/js/remotecontroller.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('remote.min.js'))
-        .pipe(gulp.dest('server/views/assets/dist'));
+        .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 gulp.task('default', function(){
-    gulp.watch('server/views/assets/js/*.js', ['js']);
-    gulp.watch('server/views/assets/js/*.js', ['embed']);
-    gulp.watch(['server/views/assets/js/callback.js', 'server/views/assets/js/helpers.js'], ['callback']);
-    //gulp.watch('server/views/assets/js/*.js', ['nochan']);
-    gulp.watch('server/views/assets/js/remotecontroller.js', ['remotecontroller']);
+    gulp.watch('server/public/assets/js/*.js', ['js']);
+    gulp.watch('server/public/assets/js/*.js', ['embed']);
+    gulp.watch(['server/public/assets/js/callback.js', 'server/public/assets/js/helpers.js'], ['callback']);
+    //gulp.watch('server/public/assets/js/*.js', ['nochan']);
+    gulp.watch('server/public/assets/js/remotecontroller.js', ['remotecontroller']);
 });
