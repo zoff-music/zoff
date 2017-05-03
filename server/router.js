d938ce31f0d277d7eda20b5ddea608785f546eab
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-05-03 18:47:57 +0200

    fixed port redirect issue

--- server/router.js
+++ server/router.js
@@ -10,34 +10,34 @@ router.use(function(req, res, next) {
 
 router.route('/:channel_name').get(function(req, res, next){
     try{
-      var url = req.headers['x-forwarded-host'];
-      var subdomain = req.headers['x-forwarded-host'].split(".");
-      if(url != "zoff.me" && url != "remote.zoff.me" && url != "remote.localhost" && url != "localhost") {
-          res.redirect("https://zoff.me");
-          return;
-      }
-      if(subdomain[0] == "remote") {
-          var data = {
-              year: 2017,
-              javascript_file: "remote.min.js"
-          }
-          res.render('layouts/remote', data);
-      } else if(subdomain.length >= 2 && subdomain[0] == "www") {
-          res.redirect("https://zoff.me");
-      } else {
-          if(req.params.channel_name == "_embed") {
-              res.sendFile(path.join(__dirname, '/views/assets/html/embed.html'));
-          } else if(req.params.channel_name == "o_callback") {
-              res.sendFile(path.join(__dirname, '/views/assets/html/callback.html'));
-          } else {
-              var data = {
-                  list_name: capitalizeFirstLetter(req.params.channel_name),
-                  year: 2017,
-                  javascript_file: "main.min.js"
-              }
-              res.render('layouts/channel', data);
-          }
-      }
+        var url = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host.split(":")[0];
+        var subdomain = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'].split(".") : req.headers.host.split(":")[0].split(".");
+        if(url != "zoff.me" && url != "remote.zoff.me" && url != "remote.localhost" && url != "localhost") {
+            res.redirect("https://zoff.me");
+            return;
+        }
+        if(subdomain[0] == "remote") {
+            var data = {
+                year: 2017,
+                javascript_file: "remote.min.js"
+            }
+            res.render('layouts/remote', data);
+        } else if(subdomain.length >= 2 && subdomain[0] == "www") {
+            res.redirect("https://zoff.me");
+        } else {
+            if(req.params.channel_name == "_embed") {
+                res.sendFile(path.join(__dirname, '/views/assets/html/embed.html'));
+            } else if(req.params.channel_name == "o_callback") {
+                res.sendFile(path.join(__dirname, '/views/assets/html/callback.html'));
+            } else {
+                var data = {
+                    list_name: capitalizeFirstLetter(req.params.channel_name),
+                    year: 2017,
+                    javascript_file: "main.min.js"
+                }
+                res.render('layouts/channel', data);
+            }
+        }
     } catch(e) {
       res.redirect("https://zoff.me");
     }
@@ -126,8 +126,8 @@ router.route('/api/mail').post(function(req, res) {
 
 router.route('/').get(function(req, res, next){
     try{
-        var url = req.headers['x-forwarded-host'];
-        var subdomain = req.headers['x-forwarded-host'].split(".");
+        var url = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host.split(":")[0];
+        var subdomain = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'].split(".") : req.headers.host.split(":")[0].split(".");
         if(url != "zoff.me" && url != "remote.zoff.me" && url != "remote.localhost" && url != "localhost") {
             res.redirect("https://zoff.me");
             return;
@@ -148,7 +148,8 @@ router.route('/').get(function(req, res, next){
             res.render('layouts/frontpage', data);
         }
     } catch(e) {
-        res.redirect("https://zoff.me");
+        console.log(e);
+        //res.redirect("https://zoff.me");
     }
 });
 
