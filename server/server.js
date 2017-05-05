f8ec4cd888bfc2aa215ad27f5693fcd6f6e34985
Kasper Rynning-Tønnesen <kasperr@stud.ntnu.no>
2017-05-05 10:48:10 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -715,7 +715,7 @@ io.on('connection', function(socket){
             var skipping = params.skipping;
             var shuffling = params.shuffling;
             var userpass = params.userpass;
-            if(!params.userpass_changed && frontpage) {
+            if(!params.userpass_changed) {
               userpass = "";
             } else if(params.userpass_changed && userpass != "") {
               frontpage = false;
