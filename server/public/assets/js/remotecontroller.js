eb3085a23c1f7ac0b036fbf093ae8a012b32dd5c
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-05-31 20:54:28 +0200

    Github commit fix

--- server/public/assets/js/remotecontroller.js
+++ server/public/assets/js/remotecontroller.js
@@ -31,19 +31,18 @@ $(document).ready(function (){
 	}
 
 	try{
-		git_info = $.ajax({
+		$.ajax({
 			type: "GET",
 			url: "https://api.github.com/users/zoff-music/received_events",
-			async: false
-		}).responseText;
-
-		git_info = $.parseJSON(git_info);
-		$("#latest-commit").html("Latest Commit: <br>" +
-		git_info[0].created_at.substring(0,10) +
-		": " + git_info[0].actor.display_login +
-		"<br><a href='https://github.com/"+git_info[0].repo.name+"/commit/" + git_info[0].payload.commits[0].sha + "' target='_blank'>" +
-		git_info[0].payload.commits[0].sha.substring(0,10) + "</a>: " +
-		git_info[0].payload.commits[0].message+"<br");
+			success: function(git_info){
+				$("#latest-commit").html("Latest Commit: <br>" +
+				git_info[0].created_at.substring(0,10) +
+				": " + git_info[0].actor.display_login +
+				"<br><a href='https://github.com/"+git_info[0].repo.name+"/commit/" + git_info[0].payload.commits[0].sha + "' target='_blank'>" +
+				git_info[0].payload.commits[0].sha.substring(0,10) + "</a>: " +
+				git_info[0].payload.commits[0].message+"<br");
+			}
+		});
 	} catch(e) {}
 });
 
