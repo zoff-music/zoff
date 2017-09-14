d0e53ec1f507d1a44daded99f1b853c692d6684d
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-09-14 12:00:12 +0200

    Fixed issue with fb.zoff.me not being able to connect

--- server/public/assets/js/listeners.js
+++ server/public/assets/js/listeners.js
@@ -206,7 +206,7 @@ function init(){
 	};
 
 	if(window.location.hostname != "fb.zoff.me") share_link_modifier_channel();
-	if(window.location.hostname == "zoff.me") add = "https://zoff.me";
+	if(window.location.hostname == "zoff.me" || window.location.hostname == "fb.zoff.me") add = "https://zoff.me";
 	else add = window.location.hostname;
 
 	if(Player !== undefined) Player.stopInterval= false;
