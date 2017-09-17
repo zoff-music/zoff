c64a890c08a8b1e1d1160f06646578c6aa64810c
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-09-17 21:23:41 +0200

    Trying to move frontpage back to io.js

--- server/handlers/io.js
+++ server/handlers/io.js
@@ -152,7 +152,20 @@ module.exports = function() {
 		socket.on('frontpage_lists', function(msg)
 		{
             console.log("First gotten", Functions.get_time());
-			Frontpage.frontpage_lists(msg, socket);
+			/*Frontpage.frontpage_lists(msg, socket);*/
+			if(!msg.hasOwnProperty('version') || msg.version != VERSION || msg.version == undefined) {
+			    socket.emit("update_required");
+			  }
+
+			console.log("gotten second ", Functions.get_time());
+			  db.collection("frontpage_lists").find({frontpage:true}, function(err, docs){
+			    console.log("gotten third ", Functions.get_time());
+			        db.collection("connected_users").find({"_id": "total_users"}, function(err, tot){
+			        console.log("sending frontpage ", Functions.get_time());
+			        socket.compress(true).emit("playlists", {channels: docs, viewers: tot[0].total_users});
+			        console.log("sent frontpage ", Functions.get_time());
+			         });
+			  });
 		});
 
 		socket.on('now_playing', function(list, fn)
