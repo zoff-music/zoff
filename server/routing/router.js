ae6caaa657b0bf46749a8c242a8d61c9974584ed
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-09-26 16:54:42 +0200

    Added more api

--- server/routing/router.js
+++ server/routing/router.js
@@ -55,6 +55,57 @@ router.route('/api/frontpages').get(function(req, res) {
     });
 });
 
+router.route('/api/list/:channel_name').get(function(req, res) {
+    var channel_name = req.params.channel_name;
+    db.collection(channel_name).find({views: {$exists: false}}, {added: 1, id: 1, title: 1, votes: 1, duration: 1, type: 1, _id: 0}, function(err, docs) {
+        if(docs.length > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(docs));
+        } else {
+            res.status(404);
+            res.send(404);
+        }
+    });
+});
+
+router.route('/api/conf/:channel_name').get(function(req, res) {
+    var channel_name = req.params.channel_name;
+    db.collection(channel_name).find({views: {$exists: true}},
+        {
+            addsongs: 1,
+            adminpass: 1,
+            allvideos: 1,
+            frontpage: 1,
+            longsongs: 1,
+            removeplay: 1,
+            shuffle: 1,
+            skip: 1,
+            startTime: 1,
+            userpass: 1,
+            vote: 1,
+            _id: 0
+        }, function(err, docs) {
+        if(docs.length > 0) {
+            var conf = docs[0];
+            if(conf.adminpass != "") {
+                conf.adminpass = true;
+            } else {
+                conf.adminpass = false;
+            }
+            if(conf.userpass != "") {
+                conf.userpass = true;
+            } else {
+                conf.userpass = false;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(conf));
+        } else {
+            res.status(404);
+            res.send(404);
+        }
+    });
+});
+
 router.route('/api/imageblob').post(function(req, res) {
     var Jimp = require("jimp");
     Jimp.read('https://img.youtube.com/vi/' + req.body.id + '/mqdefault.jpg', function (err, image) {
