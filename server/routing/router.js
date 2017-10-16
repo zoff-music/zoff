74b9fae2ed3cf2b7540555fa9605314cd0b84cb4
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-10-16 22:35:46 +0200

    Trying to support fb.zoff.me

--- server/routing/router.js
+++ server/routing/router.js
@@ -219,6 +219,72 @@ router.route('/').get(function(req, res, next){
     }
 });
 
+router.route('/').post(function(req, res, next){
+    try{
+        var url = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host.split(":")[0];
+        var subdomain = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'].split(".") : req.headers.host.split(":")[0].split(".");
+        if(url != "zoff.me" && url != "remote.zoff.me" && url != "fb.zoff.me" && url != "remote.localhost" && url != "localhost") {
+            res.redirect("https://zoff.me");
+            return;
+        }
+        if(subdomain[0] == "remote") {
+            var data = {
+                year: 2017,
+                javascript_file: "remote.min.js"
+            }
+            res.render('layouts/remote', data);
+        } else if(subdomain[0] == "www") {
+            res.redirect("https://zoff.me");
+        } else {
+            var data = {
+                year: 2017,
+                javascript_file: "main.min.js"
+            }
+            res.render('layouts/frontpage', data);
+        }
+    } catch(e) {
+        //console.log(e);
+        //res.redirect("https://zoff.me");
+    }
+});
+
+router.route('/:channel_name').post(function(req, res, next){
+    try{
+        var url = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host.split(":")[0];
+        var subdomain = req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'].split(".") : req.headers.host.split(":")[0].split(".");
+        if(url != "zoff.me" && url != "remote.zoff.me" && url != "fb.zoff.me" && url != "remote.localhost" && url != "localhost") {
+            res.redirect("https://zoff.me");
+            return;
+        }
+        if(subdomain[0] == "remote") {
+            var data = {
+                year: 2017,
+                javascript_file: "remote.min.js"
+            }
+            res.render('layouts/remote', data);
+        } else if(subdomain.length >= 2 && subdomain[0] == "www") {
+            res.redirect("https://zoff.me");
+        } else {
+            if(req.params.channel_name == "_embed") {
+                res.sendFile(path.join(pathThumbnails, '/public/assets/html/embed.html'));
+            } else if(req.params.channel_name == "o_callback") {
+                res.sendFile(path.join(pathThumbnails, '/public/assets/html/callback.html'));
+            } else {
+                var data = {
+                    list_name: capitalizeFirstLetter(req.params.channel_name),
+                    year: 2017,
+                    javascript_file: "main.min.js"
+                }
+                res.render('layouts/channel', data);
+            }
+        }
+    } catch(e) {
+      res.redirect("https://zoff.me");
+    }
+});
+
+
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
