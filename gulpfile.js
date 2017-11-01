787ea6f1e4d604fdc2ee355d44926e71aa34d82c
Kasper Rynning-Tønnesen <kasperr@stud.ntnu.no>
2017-11-01 11:59:37 +0100

    Revert "Trying to concat libs"

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,8 @@ var gulp    = require('gulp'),
 	uglify  = require('gulp-uglifyjs'),
 	concat  = require('gulp-concat');
 
-gulp.task('lib', function () {
-    gulp.src(['server/public/assets/dist/lib/jquery-2.2.4.min.js', 'server/public/assets/dist/lib/materialize.min.js', 'server/public/assets/dist/lib/jquery-ui.min.js', 'server/public/assets/dist/lib/jquery.contextMenu.js', 'server/public/assets/dist/lib/jquery.ui.position.min.js', 'server/public/assets/dist/lib/jquery.ui.touch-punch.min'])
-        .pipe(uglify({
-        	mangle: true,
-            compress: true,
-        	enclose: true
-        }))
-        .pipe(concat('lib.min.js'))
-        .pipe(gulp.dest('server/public/assets/dist'));
-});
-
 gulp.task('js', function () {
-    gulp.src(['server/public/assets/js/lib/*.js','server/public/assets/js/*.js', '!server/public/assets/js/embed*', '!server/public/assets/js/remotecontroller.js', '!server/public/assets/js/callback.js'])
+    gulp.src(['server/public/assets/js/*.js', '!server/public/assets/js/embed*', '!server/public/assets/js/remotecontroller.js', '!server/public/assets/js/callback.js'])
         .pipe(uglify({
         	mangle: true,
             compress: true,
@@ -48,11 +37,11 @@ gulp.task('callback', function () {
 });
 
 gulp.task('build', function() {
-    gulp.run(['lib', 'js', 'embed', 'remotecontroller', 'callback']);
+    gulp.run(['js', 'embed', 'remotecontroller', 'callback']);
 })
 
 gulp.task('remotecontroller', function () {
-    gulp.src(['server/public/assets/js/lib/*.js', 'server/public/assets/js/remotecontroller.js'])
+    gulp.src(['server/public/assets/js/remotecontroller.js'])
         .pipe(uglify({
             mangle: true,
             compress: true,
@@ -63,7 +52,6 @@ gulp.task('remotecontroller', function () {
 });
 
 gulp.task('default', function(){
-    gulp.watch('server/public/assets/dist/lib/*.js', ['lib']);
     gulp.watch('server/public/assets/js/*.js', ['js']);
     gulp.watch('server/public/assets/js/*.js', ['embed']);
     gulp.watch(['server/public/assets/js/callback.js', 'server/public/assets/js/helpers.js'], ['callback']);
