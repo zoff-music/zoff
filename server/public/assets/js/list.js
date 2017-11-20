3afb93a21a041962073f2832634dd51cf3bd661c
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2017-11-20 09:33:09 +0100

    Forgot to format logging correctly

--- server/public/assets/js/list.js
+++ server/public/assets/js/list.js
@@ -301,13 +301,13 @@ var List = {
     },
 
     check_error_videos: function(i) {
-        Helper.log("Empty-checker at", i);
+        Helper.log("Empty-checker at " + i);
             $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + full_playlist[i].id
                        + "&key=" + api_key + "&part=snippet",
               function (data, status, xhr) {
-                  Helper.log("Empty-checker items", data.items.length);
+                  Helper.log("Empty-checker items " + data.items.length);
                 if (data.items.length == 0) {
-                    Helper.log("Emtpy-checker error at", full_playlist[i].id, full_playlist[i].title);
+                    Helper.log("Emtpy-checker error at " + full_playlist[i].id + " " + full_playlist[i].title);
                     socket.emit("error_video", {channel: chan.toLowerCase(), id: full_playlist[i].id, title: full_playlist[i].title});
                 }
                 if(full_playlist.length > i + 1 && window.location.pathname != "/") {
@@ -315,7 +315,7 @@ var List = {
                 }
 
             }).error(function (xhr, errorType, exception) {
-                Helper.log("Emtpy-checker error at", full_playlist[i].id, full_playlist[i].title);
+                Helper.log("Emtpy-checker error at " + full_playlist[i].id + " " + full_playlist[i].title);
                 socket.emit("error_video", {channel: chan.toLowerCase(), id: full_playlist[i].id, title: full_playlist[i].title});
                 if(full_playlist.length > i + 1 && window.location.pathname != "/") {
                     List.check_error_videos(i + 1);
