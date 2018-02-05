cd491441f0fb356132f47b2a4779192f8702564b
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-02-05 14:01:53 +0100

    More trying and failing

--- server/app.js
+++ server/app.js
@@ -1,35 +1,73 @@
-var app = require('./index.js');
-var path = require('path');
-var publicPath = path.join(__dirname, 'public');
-var debug = require('debug')('king:server');
-var http = require('http');
-//var config = require('../config/index');
+var express = require('express'),
+    cluster = require('cluster'),
+    net = require('net'),
+    socketio = require('socket.io'),
+    socket_redis = require('socket.io-redis');
 
-/**
-* Get port from environment and store in Express.
-*/
-//var env = app.get('env');
-var port = 8080;
-app.set('port', port);
+    var path = require('path');
+    var publicPath = path.join(__dirname, 'public');
 
-var cluster = require('cluster');
+    var http = require('http');
+
+var port = 8080,
+    num_processes = require('os').cpus().length;
+
 if (cluster.isMaster) {
-    var numWorkers = require('os').cpus().length;
-    for (var i = 0; i < numWorkers; i++) cluster.fork();
-    console.log('[CLUSTER] Master cluster setting up ' + numWorkers + ' workers...');
+    // This stores our workers. We need to keep them to be able to reference
+    // them based on source IP address. It's also useful for auto-restart,
+    // for example.
+    var workers = [];
 
-    cluster.on('online', function(worker) {
-        console.log('[CLUSTER] Worker ' + worker.process.pid + ' is online');
-    });
-    cluster.on('exit', function(worker, code, signal){
-        console.log('[CLUSTER] Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('[CLUSTER] Starting a new worker');
-        cluster.fork();
-    });
+    // Helper function for spawning worker at index 'i'.
+    var spawn = function(i) {
+        workers[i] = cluster.fork();
+
+        // Optional: Restart worker on exit
+        workers[i].on('exit', function(code, signal) {
+            console.log('respawning worker', i);
+            spawn(i);
+        });
+    };
+
+    // Spawn workers.
+    for (var i = 0; i < num_processes; i++) {
+        spawn(i);
+    }
+
+    // Helper function for getting a worker index based on IP address.
+    // This is a hot path so it should be really fast. The way it works
+    // is by converting the IP address to a number by removing non numeric
+  // characters, then compressing it to the number of slots we have.
+    //
+    // Compared against "real" hashing (from the sticky-session code) and
+    // "real" IP number conversion, this function is on par in terms of
+    // worker index distribution only much faster.
+    var worker_index = function(ip, len) {
+        var s = '';
+        for (var i = 0, _len = ip.length; i < _len; i++) {
+            if (!isNaN(ip[i])) {
+                s += ip[i];
+            }
+        }
+
+        return Number(s) % len;
+    };
+
+    // Create the outside facing server listening on our port.
+    var server = net.createServer({ pauseOnConnect: true }, function(connection) {
+        // We received a connection and need to pass it to the appropriate
+        // worker. Get the worker for this connection's source IP and pass
+        // it the connection.
+        var worker = workers[worker_index(connection.remoteAddress, num_processes)];
+        worker.send('sticky-session:connection', connection);
+    }).listen(port);
 } else {
-    /**
-    * Create HTTP server.
-    */
+    // Note we don't use a port here because the master listens on it for us.
+    var app = require('./index.js');
+
+    // Here you might use middleware, attach routes, etc
+
+    // Don't expose our internal server to the outside.
     try {
         var cert_config = require(path.join(path.join(__dirname, 'config'), 'cert_config.js'));
     	var fs = require('fs');
@@ -65,58 +103,45 @@ if (cluster.isMaster) {
     	});
     	var http = require('http');
     	server = http.Server(app);
-    	add = ",http://localhost:80*,http://localhost:8080*,localhost:8080*, localhost:8082*,http://zoff.dev:80*,http://zoff.dev:8080*,zoff.dev:8080*, zoff.dev:8082*";
-    }
-    var port = 8080;
-
-    /**
-    * Listen on provided port, on all network interfaces.
-    */
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-
-    /**
-    * Socket.io
-    */
-    var io = app.socketIO;
-    io.listen(server);
-    //socketIO.set('transports', ['websocket']);
-}
-
-
-/**
-* Event listener for HTTP server "error" event.
-*/
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
+    	//add = ",http://localhost:80*,http://localhost:8080*,localhost:8080*, localhost:8082*,http://zoff.dev:80*,http://zoff.dev:8080*,zoff.dev:8080*, zoff.dev:8082*";
     }
 
-    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    server.listen();
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-        case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-        default:
-        throw error;
-    }
-}
+    var socketIO = app.socketIO;
+    var redis = require('socket.io-redis');
+    socketIO.adapter(redis({ host: 'localhost', port: 6379 }));
+    //socketIO.set( 'origins', '*localhost:8080' );
+    socketIO.listen(server);
 
-/**
-* Event listener for HTTP server "listening" event.
-*/
+    // Tell Socket.IO to use the redis adapter. By default, the redis
+    // server is assumed to be on localhost:6379. You don't have to
+    // specify them explicitly unless you want to change them.
+    //io.adapter(socket_redis({ host: 'localhost', port: 6379 }));
 
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+    // Here you might use Socket.IO middleware for authorization etc.
+
+    /*io.on('connection', function(socket) {
+        console.log('New client connection detected on process ' + process.pid);
+
+        socket.emit('welcome', {message: 'Welcome to BlueFrog Chat Room'});
+        socket.on('new.message', function(message) {
+            socket.emit('new.message', message);
+        })
+
+    });*/
+
+
+    // Listen to messages sent from the master. Ignore everything else.
+    process.on('message', function(message, connection) {
+        if (message !== 'sticky-session:connection') {
+            return;
+        }
+
+        // Emulate a connection event on the server by emitting the
+        // event with the connection the master sent us.
+        server.emit('connection', connection);
+
+        connection.resume();
+    });
 }
