63d76a48b36cbd3f9f9e6e0213c576e06cd10189
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-02-25 22:53:21 +0100

    Added some debuglogs for hostcontroller

--- server/public/assets/js/hostcontroller.js
+++ server/public/assets/js/hostcontroller.js
@@ -5,6 +5,8 @@ var Hostcontroller = {
     old_id: null,
 
     host_listener: function(id) {
+        Helper.log("Host-listener triggered");
+        Helper.log("Host-listener id:", id);
         if(Hostcontroller.old_id === null) Hostcontroller.old_id = id;
         else {
             socket.removeAllListeners(id);
