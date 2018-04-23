6dabe2963e6156a9a0c9b7fed3b52969168b7f3e
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-04-23 16:49:44 +0200

    using correct api-token on live

--- server/public/assets/js/listeners.js
+++ server/public/assets/js/listeners.js
@@ -30,8 +30,10 @@ var showDiscovery						= false;
 var player_ready 	   	  		= false;
 var viewers 			  		= 1;
 var temp_user_pass 				= "";
-//var zoff_api_token = "DwpnKVkaMH2HdcpJT2YPy783SY33byF5/32rbs0+xdU=";
-var zoff_api_token = "AhmC4Yg2BhaWPZBXeoWK96DAiAVfbou8TUG2IXtD3ZQ=";
+var zoff_api_token = "DwpnKVkaMH2HdcpJT2YPy783SY33byF5/32rbs0+xdU=";
+if(window.location.hostname == "localhost" || window.location.hostname == "client.localhost") {
+    var zoff_api_token = "AhmC4Yg2BhaWPZBXeoWK96DAiAVfbou8TUG2IXtD3ZQ=";
+}
 var retry_frontpage;
 var chromecast_specs_sent = false;
 var dragging 					= false;
