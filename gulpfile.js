e4f90e20fd4927e3c3285201cb994c87d0a683f5
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-07-06 00:56:15 +0200

    No map files maybe

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp    = require('gulp'),
 
 gulp.task('js', function () {
     return gulp.src(['server/VERSION.js', 'server/config/api_key.js', 'server/public/assets/js/*.js', '!server/public/assets/js/embed*', '!server/public/assets/js/token*', '!server/public/assets/js/remotecontroller.js', '!server/public/assets/js/callback.js'])
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(concat('main.min.js'))
         .pipe(uglify({
         	mangle: true,
@@ -14,46 +14,46 @@ gulp.task('js', function () {
         	enclose: true,
         }))
 
-        .pipe(sourcemaps.write('maps'))
+        //.pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 gulp.task('embed', function () {
     return gulp.src(['server/VERSION.js', 'server/config/api_key.js', 'server/public/assets/js/player.js', 'server/public/assets/js/helpers.js', 'server/public/assets/js/playercontrols.js', 'server/public/assets/js/list.js', 'server/public/assets/js/embed.js', '!server/public/assets/js/frontpage*', '!server/public/assets/js/remotecontroller.js', 'server/public/assets/js/hostcontroller.js'])
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('embed.min.js'))
-        .pipe(sourcemaps.write('maps'))
+        //.pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('server/public/assets/dist'));
 });
 
 gulp.task('token', function() {
     return gulp.src(['server/public/assets/js/token*', 'server/public/assets/js/helpers.js'])
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('token.min.js'))
-        .pipe(sourcemaps.write('maps'))
+        //.pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('server/public/assets/dist'));
 })
 
 gulp.task('callback', function () {
     return gulp.src(['server/VERSION.js', 'server/config/api_key.js', 'server/public/assets/js/callback.js'])
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('callback.min.js'))
-        .pipe(sourcemaps.write('maps'))
+        //.pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('server/public/assets/dist'));
 });
 
@@ -63,14 +63,14 @@ gulp.task('build', function() {
 
 gulp.task('remotecontroller', function () {
     return gulp.src(['server/VERSION.js', 'server/config/api_key.js', 'server/public/assets/js/remotecontroller.js', 'server/public/assets/js/helpers.js'])
-    .pipe(sourcemaps.init())
+    ////.pipe(sourcemaps.init())
         .pipe(uglify({
             mangle: true,
             compress: true,
             enclose: true
         }))
         .pipe(concat('remote.min.js'))
-        .pipe(sourcemaps.write('maps'))
+        //.pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('server/public/assets/dist'));
 });
 
