589c690871d757e6362518ef613bcf5d94891f73
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-08-10 11:59:49 +0200

    Fixed frontpage-sorting issue

--- server/public/assets/js/channel.js
+++ server/public/assets/js/channel.js
@@ -681,7 +681,12 @@ var Channel = {
                     var newList = response.querySelector("#lists-script").innerHTML;
                     newList = newList.trim().replace("window.lists = ", "");
                     newList = newList.substring(0, newList.length);
-                    window.lists = JSON.parse(newList);
+                    try {
+                        window.lists = JSON.parse(newList);
+                    } catch(e) {
+                        console.error(e);
+                        console.log(newList);
+                    }
                     response.querySelector("#lists-script").remove();
                     Helper.removeElement("#sidenav-overlay");
                     document.getElementsByTagName("main")[0].className = "center-align container";
