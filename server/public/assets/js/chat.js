7abdba755342d00b9818b563f2356e949fa86a22
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2018-09-20 16:48:42 +0200

    Fixed newly created issue of timestamps of chat

--- server/public/assets/js/chat.js
+++ server/public/assets/js/chat.js
@@ -58,6 +58,10 @@ var Chat = {
                 add = "chatchannel";
             }
             for(var x = 0; x < Chat.chat_help.length; x++) {
+
+                var _time = new Date();
+                var time = Helper.pad(_time.getHours()) + ":" + Helper.pad(_time.getMinutes());
+
                 document.querySelector("#" + add).insertAdjacentHTML("beforeend", "<li title='Zoff''><span class='time_color'>" + time + "</span> <img class='chat-icon' src='https://zoff.me/assets/images/favicon-32x32.png' alt='System'><span style='color:orange;'>System</span>: </li>");
                 var in_text = document.createTextNode(Chat.chat_help[x]);
                 document.querySelector("#" + add).children[document.querySelector("#" + add).children.length - 1].appendChild(in_text);
